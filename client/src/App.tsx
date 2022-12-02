652d056c189d38ee63d69f8e12cbe39d5f87699f
akishtp <makishtp@gmail.com>
2022-12-02 19:27:00 +0530

    DSingle Question ☑️

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,22 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./Pages/Home/Home";
+import Test from "./Pages/Test/Test";
 
 const App: React.FC = () => {
   return (
-    <div className="App">
+    <BrowserRouter>
       <Navbar />
-      <Home />
-    </div>
+      <div className="App">
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="/test/:id" element={<Test />} />
+          <Route path="*" element={<span>$404 Error</span>} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 };
 
--- client/src/Pages/Home/Home.tsx
+++ client/src/Pages/Home/Home.tsx
@@ -1,23 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import "./Home.css";
 
 const Home: React.FC = () => {
+  const [tcode, setTcode] = useState("");
+  const navigate = useNavigate();
+  const submitHandler = () => {
+    navigate(`/test/${tcode}`);
+  };
   return (
     <div className="home">
-      <header>Enter your Details</header>
-      <form>
-        <label>
-          Register Number :
-          <input type="text" placeholder="21BCS011" />
-        </label>
-        <label>
-          Password : <input type="password" placeholder="dd/mm/yyyy" />
-        </label>
-        <label>
-          Test Code : <input type="text" placeholder="Ask your Teacher" />
-        </label>
-        <button type="submit">Start Test</button>
-      </form>
+      <div className="home-container">
+        <header>Enter your Details</header>
+        <form onSubmit={() => submitHandler()}>
+          <label>
+            Register Number :
+            <input type="text" placeholder="eg : 21BCS011" />
+          </label>
+          {/* <label>
+            Password : <input type="password" placeholder="dd/mm/yyyy" />
+          </label> */}
+          <label>
+            Test Code :
+            <input
+              type="text"
+              placeholder="Ask your Teacher"
+              value={tcode}
+              onChange={(e) => setTcode(e.target.value)}
+              required
+            />
+          </label>
+          <button type="submit">Start Test</button>
+        </form>
+      </div>
     </div>
   );
 };
